0b970d7b9c68d44d180dc5ef161f46900647bcbd
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-22 15:23:46 +0300

    Created UI.

---
+++ GCDProject/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  GCDProject
+//
+//  Created by Lera Savchenko on 22.09.23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
