705d45f763723ff207dbd06f5cc39523cf5d607d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-22 15:17:05 +0300

    Initial Commit

---
+++ GCDProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCDProject
+//
+//  Created by Lera Savchenko on 22.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
